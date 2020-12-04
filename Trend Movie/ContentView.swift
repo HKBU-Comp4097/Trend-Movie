6424d22715adaf6759a47ee2755f9103d1d47f3e
WesleyCKY <wesleychin9966@gmail.com>
2020-12-04 22:56:48 +0800

    Initial Commit

---
+++ Trend Movie/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Trend Movie
+//
+//  Created by Wesley chin on 4/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
