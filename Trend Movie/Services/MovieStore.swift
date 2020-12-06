4fea4ed2d0f631e5d499591ca638449163f0e5e8
WesleyCKY <wesleychin9966@gmail.com>
2020-12-06 17:53:42 +0800

    support decoding json, complete function on handling fetching and search movie

---
+++ Trend Movie/Services/MovieStore.swift
@@ -0,0 +1,103 @@
+//
+//  MovieStore.swift
+//  Trend Movie
+//
+//  Created by Wesley chin on 4/12/2020.
+//
+
+import Foundation
+
+class MovieStore: MovieService {
+    static let shared = MovieStore()
+    private init() {}
+    
+    private let apiKey = "c4b8b0514740874e0f5fc6000ebd6b7f"  //https://www.themoviedb.org
+    private let baseAPIURL = "https://api.themoviedb.org/3"
+    private let urlSession = URLSession.shared
+    private let jsonDecoder = Utils.jsonDecoder
+    
+    //fetch movies func
+    func fetchMovies(from endPoint: MovieListEndPoint, completion: @escaping (Result<MovieResponse, MovieError>) -> ()){
+        guard let url = URL(string: "\(baseAPIURL)/movie/\(endPoint.rawValue)") else {
+            completion(.failure(.invalidEndpoint))
+            return
+        }
+        self.loadURLAndDecode(url: url, completion: completion)
+    }
+    // fetch a movie func
+    func fetchMovie(id: Int, completion: @escaping (Result<Movie, MovieError>) -> ()){
+        guard let url = URL(string: "\(baseAPIURL)/movie/\(id)") else {
+            completion(.failure(.invalidEndpoint))
+            return
+        }
+        self.loadURLAndDecode(url: url, params: [
+            "append_to_response": "videos,credits"
+        ], completion: completion)
+    }
+    // search for movie(s)
+    func searchMovie(query: String, completion: @escaping (Result<MovieResponse, MovieError>) -> ()){
+        guard let url = URL(string: "\(baseAPIURL)/search/movie") else {
+            completion(.failure(.invalidEndpoint))
+            return
+        }
+        self.loadURLAndDecode(url: url, params: [
+            "language": "en-US",
+            "include_adult": "false",
+            "region": "US",
+            "query": query
+        ], completion: completion)
+    }
+    // load the URL and perform decode
+    private func loadURLAndDecode<D: Decodable>(url: URL, params: [String: String]? = nil, completion: @escaping (Result<D, MovieError>) -> ()){
+        
+        guard var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false) else {
+            completion(.failure(.invalidEndpoint))
+            return
+        }
+        
+        var queryItems = [URLQueryItem(name: "api_key", value: apiKey)]
+        if let params = params {
+            queryItems.append(contentsOf: params.map { URLQueryItem(name: $0.key, value: $0.value) })
+        }
+        
+        urlComponents.queryItems = queryItems
+        
+        guard let finalURL = urlComponents.url else {
+            completion(.failure(.invalidEndpoint))
+            return
+        }
+        
+        urlSession.dataTask(with: finalURL) { [weak self] (data, response, error) in
+            guard let self = self else { return }
+
+            if error != nil {
+                self.executeCompletionHandlerInMainThread(with: .failure(.apiError), completion: completion)
+                return
+            }
+
+            guard let httpResponse = response as? HTTPURLResponse, 200..<300 ~= httpResponse.statusCode else {
+                self.executeCompletionHandlerInMainThread(with: .failure(.invalidResponse), completion: completion)
+                return
+            }
+
+            guard let data = data else {
+                self.executeCompletionHandlerInMainThread(with: .failure(.noData), completion: completion)
+                return
+            }
+
+            do {
+                let decodedResponse = try self.jsonDecoder.decode(D.self, from: data)
+                self.executeCompletionHandlerInMainThread(with: .success(decodedResponse), completion: completion)
+            } catch {
+                self.executeCompletionHandlerInMainThread(with: .failure(.serializationError), completion: completion)
+            }
+        }.resume()
+    }
+    
+    private func executeCompletionHandlerInMainThread<D: Decodable>(with result: Result<D, MovieError>, completion: @escaping (Result<D, MovieError>) -> ()) {
+        DispatchQueue.main.async {
+            completion(result)
+        }
+    }
+    
+}
